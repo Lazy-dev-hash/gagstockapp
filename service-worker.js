291924faae50d7d2e85ce88057e26d9aaa9561b2
Lazy-dev-hash <nelzys52@gmail.com>
2025-06-04 23:20:16 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,24 @@
-self.addEventListener('install', (event) => {
+const CACHE_NAME = "gagstock-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png",
+  "https://cdn.jsdelivr.net/npm/chart.js",
+  "https://cdn.lordicon.com/lordicon.js",
+  "https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js",
+  "https://cdn.jsdelivr.net/npm/tsparticles@2/tsparticles.bundle.min.js"
+];
+
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open('gagstock-cache-v1').then((cache) => {
-      return cache.addAll(['/', '/index.html', '/favicon.svg']);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
